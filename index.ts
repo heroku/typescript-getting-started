a7c4fcd5f320fdb8217968e08d3a5814e940615c
Danielle Adams <danielle.adams@salesforce.com>
2021-01-20 16:22:11 -0500

    add styled landing page

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
 import express from 'express';
+import path from 'path';
 
 const PORT = process.env.PORT || 5000;
 const server = express();
 
-server.get('/', (_: express.Request, res: express.Response) => {
-  res.send("Hello World!");
-});
-
-server.listen(PORT, () => {
-  console.log(`Listening on ${PORT}`);
-});
+express()
+  .use(express.static(path.join(__dirname, '../public')))
+  .set('views', path.join(__dirname, '../views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
